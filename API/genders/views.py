ebf5bd4b57d118cde075fef52a79f78f922e4645
constanzajazme <constanza.jazmeh@utem.cl>
2019-07-18 03:23:23 -0400

    Funcion de edad incompleta

--- API/genders/views.py
+++ API/genders/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from genders.models import Gender
 from genders.serializers import GenderSerializer
-from python_code.spchtest import recognize_gender
+from python_code.spchtest import recognize_gender, myspf0med, get_age
 
 from rest_framework import generics
 import base64
@@ -18,5 +18,11 @@ class GendersList(generics.ListCreateAPIView):
     serializer_class= GenderSerializer
 
 def prueba1(request):
-    value= recognize_gender()
-    return HttpResponse('<h1> El audio recibido es '+ value +'</h1>')
+    value= recognize_gender('probando.wav')
+    p="probando" # Audio File title
+    c="API/python_code/testing_file" # Path to the Audio_File directory (Python 3.7)
+    hz=myspf0med(p,c)
+    result=get_age(hz,value)
+
+
+    return HttpResponse('<h1> El audio recibido es '+ result +'</h1>')
